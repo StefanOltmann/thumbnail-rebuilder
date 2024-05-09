4f7303296216b8c669df038293497b2f8d15e9ba
Stefan Oltmann <github@stefan-oltmann.de>
2024-05-09 23:57:04 +0200

    Update to Kim v0.18.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ kotlin {
     sourceSets {
 
         commonMain.dependencies {
-            api("com.ashampoo:kim:0.18")
+            api("com.ashampoo:kim:0.18.1")
             api("org.jetbrains.skiko:skiko:$skikoVersion")
         }
 
